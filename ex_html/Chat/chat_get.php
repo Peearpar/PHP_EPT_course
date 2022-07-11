6fd8d8563a23d32fc0a6a3a7848b8027b9496caf
Peearpar <phusit_sawat@hotmail.com>
2022-07-11 21:05:07 +0700

    no message

--- ex_html/Chat/chat_get.php
+++ ex_html/Chat/chat_get.php
@@ -1,7 +1,22 @@
 <?php
     $filename = "chat.txt";
     $content = file_get_contents ($filename);
+    $lines = explode("\r\n", $content);
+    $data = array();
+    for($i = 0; $i < count($lines) ; $i++)
+    {
+        $d = explode(":",$lines[$i]);
 
-    echo $content;
+        $dd = array();
+        array_push($dd, $d[0],$d[1]);
+        array_push($data, $dd);
+    }
+    $jason = json_encode($data);
+    echo $jason;
+    // print_r($data);
+    // echo "<br/>";
+    // echo "<br/>";
+    // echo "<br/>";
+    // echo $content;
 ?>
 
